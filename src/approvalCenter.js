7af34140d250f6cfe4d5b2dc0e50f85c450a278f
gargmanash <172600052+gargmanash@users.noreply.github.com>
2025-05-25 16:37:33 +0530

    changes

--- src/approvalCenter.js
+++ src/approvalCenter.js
@@ -1,13 +1,10 @@
-// eslint-disable-next-line no-console
-console.log('APPROVALCENTER.JS SCRIPT LOADED AND PARSING')
-
 /**
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-// import Vue from 'vue'
-// import ApprovalCenterView from './views/ApprovalCenterView.vue'
+import Vue from 'vue'
+import ApprovalCenterView from './views/ApprovalCenterView.vue'
 
 // Import other necessary Nextcloud Vue components or libraries if needed
 // e.g., import store from '@nextcloud/vuex'
@@ -16,8 +13,7 @@ console.log('APPROVALCENTER.JS SCRIPT LOADED AND PARSING')
 const initApprovalCenter = () => {
 	const el = document.getElementById('approval-center-vue-root')
 	if (el) {
-		el.textContent = 'JavaScript reached initApprovalCenter and found the root element.'
-		/* const app = new Vue({
+		const app = new Vue({
 			render: h => h(ApprovalCenterView),
 		})
 
@@ -27,7 +23,7 @@ const initApprovalCenter = () => {
 		// Register Nextcloud components globally if needed, e.g.:
 		// app.component('NcButton', NcButton)
 
-		app.$mount(el) */
+		app.$mount(el)
 	} else {
 		console.error('Approval Center: Could not find root element #approval-center-vue-root')
 	}
