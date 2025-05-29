9c69d6a4409d88b6b294d3605bf3694b6941007d
gargmanash <172600052+gargmanash@users.noreply.github.com>
2025-05-30 03:14:02 +0530

    fix

--- src/views/ApprovalAnalytics.vue
+++ src/views/ApprovalAnalytics.vue
@@ -252,7 +252,7 @@ export default {
 
 <style scoped lang="scss">
 #approval-analytics-page {
-	padding: 32px; /* Increased padding for more breathing room */
+	padding: 40px; /* Further increased padding */
 	display: flex;
 	flex-direction: column;
 	height: 100%; // Fill the area provided by NcAppContent
